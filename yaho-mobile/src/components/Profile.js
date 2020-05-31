436de00cc17bce44d2fd70eded4f103050f52210
zlobste <mykola.krainiuk@nure.ua>
2020-05-31 13:49:20 +0300

    + userInfo in 'Profile' screen

--- yaho-mobile/src/components/Profile.js
+++ yaho-mobile/src/components/Profile.js
@@ -1,12 +1,71 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React , { useState, useEffect }from 'react';
+import {Alert, StyleSheet, Text, View} from 'react-native';
 import AsyncStorage from "@react-native-community/async-storage";
+import config from "../../config/default";
 
-export default function Profile({ navigation }) {
+export default function Profile({ route, navigation }) {
+
+    const [user, setUser] = useState({
+
+        email: '',
+        firstName:'' ,
+        lastName: '',
+        phoneNumber: '',
+        description: ''
+    });
+
+
+    const getInfo = async () => {
+        try{
+
+            const token = await AsyncStorage.getItem('jwt');
+
+            if(token) {
+
+                const url = config.url + '/api/Customers/my-customer-info';
+
+                const response = await fetch(url, {
+                    method: 'GET',
+                    mode: 'cors',
+                    cache: 'no-cache',
+                    credentials: 'same-origin',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${token}`
+                    },
+                    redirect: 'follow',
+                    referrerPolicy: 'no-referrer',
+                });
+
+                if (response.ok) {
+
+                    const result = await response.json();
+                    setUser(result.userInfo);
+                    return result;
+                }
+            }
+        }catch (e) {
+            console.log(e);
+        }
+    };
+
+
+    useEffect(() => {
+
+        async function fetchData(){
+            const data =  await getInfo();
+        }
+       fetchData();
+
+    },[]);
+
+
 
     return (
         <View style={styles.container}>
-            <Text style={styles.text}> Hello, </Text>
+            <Text style={styles.text}>Hello, {user.firstName} {user.lastName}</Text>
+            <Text style={styles.text}>email: {user.email}</Text>
+            <Text style={styles.text}>mobile: {user.phoneNumber}</Text>
         </View>
     );
 }
